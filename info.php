8df7f149957085601ebd165eeec6f920b44bae2c
Juliusaula <117166967+Juliusaula@users.noreply.github.com>
2024-09-03 13:03:54 +0300

    Add files via upload
    nya filer

---
+++ info.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test php info</title>
+</head>
+<body>
+
+<?php
+
+$y = "Julius";
+
+
+?>
+
+    
+</body>
+</html>
---
+++ php_ovn_1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test php info</title>
+</head>
+<body>
+
+<?php
+
+$myschool = "Prakticum";
+
+$heltal = 4;
+
+$decimaltal = 2.5;
+
+$resultat = $heltal * $decimaltal;
+
+echo "$heltal * $decimaltal = $resultat<br>";
+
+
+echo $myschool;
+
+
+?>
+
+    
+</body>
+</html>
